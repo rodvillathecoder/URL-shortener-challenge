bcb929ce05787a1cb61abf112bf5f6f1666b1955
rodvillathecoder <raulrodvilla95@gmail.com>
2023-03-23 03:01:25 +0100

    URL-Shortener
    The full Project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Advance from "./components/Advance";
+import Boost from "./components/Boost";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Shortener from "./components/Shortener";
+import Showcase from "./components/Showcase";
+
+export default function App() {
+  return (
+    <>
+      <Header/>
+      <Showcase/>
+      <Shortener/>
+      <Advance/>
+      <Boost/>
+      <Footer/>
+    </>
+  )
+}
+
